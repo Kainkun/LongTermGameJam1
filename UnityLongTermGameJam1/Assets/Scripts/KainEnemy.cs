524daf32e22a50f9338d91f9168748335e90bfbc
Kainkun <kainkun1000@gmail.com>
2019-03-29 19:47:45 -0700

    Added KainShepard scene and example enemy

---
+++ UnityLongTermGameJam1/Assets/Scripts/KainEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KainEnemy : MonoBehaviour
+{
+    public float speed = 1;
+    public float distance = 1;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(0,startPosition.y + Mathf.Sin(Time.time * speed) * distance, 0);
+    }
+}
