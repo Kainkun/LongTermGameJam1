3d0bf689227611f9a0d0d1ce086181054e21d72a
Carter <andrewscart@gmail.com>
2019-03-30 17:32:03 -0700

    added player prefab and movement script (WIP)

---
+++ UnityLongTermGameJam1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public SpriteRenderer mainShipRenderer;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainShipRenderer == null)
+        {
+            mainShipRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
