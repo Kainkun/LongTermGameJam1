5b59b9da9512a7ae1552a0bf821f2ca1b724ec9d
Jacob <goldenneedham@gmail.com>
2019-04-02 13:03:00 -0700

    Added the enemy spawner, working on making the base class functionality and and custom interface

---
+++ UnityLongTermGameJam1/Assets/Editor/EnemySpawnerEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(EnemySpawner))]
+[CanEditMultipleObjects]
+public class EnemySpawnerEditor : Editor
+{
+    SerializedProperty enemySpawner;
+
+    void OnEnable()
+    {
+        enemySpawner = serializedObject.FindProperty("EnemySpawner");
+    }
+​
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(enemySpawner);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ UnityLongTermGameJam1/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyToSpawn;
+
+    [SerializeField]
+    private bool useTimedSpawner = false;
+    [SerializeField]
+    private float timeBetweenSpawns = 1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
