479e58d2c1330ff31e709835fb76b1b39e14ba5d
rho-selynn <roradee47@gmail.com>
2019-04-03 08:56:02 -0700

    Box Enemy moves from (10,0) to (-10,0)

--- UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
+++ UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class BoxEnemyIntenseEyebrows : MonoBehaviour
 {
     public Vector3 targetPosition; // declaring the Vector3 variable
-    //public float speed = 1;
-    //public float distance = 1;
+   
+    public float speed = 1;
 
 	// Start is called before the first frame update
 	void Start()
 	{
         Debug.Log("My goal is to get the sprite to move in the -x direction!");
 
-        targetPosition = transform.position;
-        transform.position = new Vector3(0, 0); // putting the sprite at (0,0)
+        //targetPosition = transform.position;
+        transform.position = new Vector3(10, 0); // putting the sprite at (10,0)
     }
 
 	// Update is called once per frame
 	void Update()
 	{
-       // transform.position = new Vector3(startPosition.x*Time.deltaTime*speed*distance, 0);
-	}
+        if (transform.position.x > 0) // if sprite position is greater than 0 in x-direction
+        {
+            targetPosition.x = -10; // target position will be -10 in x-direction
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime); // Vector3 will move to target position
+                                                                                                              // from orginal position
+                                                                                                              // Idk what speed * Time.deltaTime does tho
+                                                                                                              // It just worked! :D
+
+    }
 }
