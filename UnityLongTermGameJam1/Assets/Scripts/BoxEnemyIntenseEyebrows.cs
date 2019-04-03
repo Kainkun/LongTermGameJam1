ec28984fe16ff7f02ffbe0a3567076c14643590c
rho-selynn <roradee47@gmail.com>
2019-04-02 18:33:32 -0700

    Made script so BoxEnemyIntenseEyebrows is at (0,0)

--- UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
+++ UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BoxEnemyIntenseEyebrows : MonoBehaviour
 {
+    public Vector3 targetPosition; // declaring the Vector3 variable
+    //public float speed = 1;
+    //public float distance = 1;
+
 	// Start is called before the first frame update
 	void Start()
 	{
+        Debug.Log("My goal is to get the sprite to move in the -x direction!");
 
-	}
+        targetPosition = transform.position;
+        transform.position = new Vector3(0, 0); // putting the sprite at (0,0)
+    }
 
 	// Update is called once per frame
 	void Update()
 	{
-
+       // transform.position = new Vector3(startPosition.x*Time.deltaTime*speed*distance, 0);
 	}
 }
