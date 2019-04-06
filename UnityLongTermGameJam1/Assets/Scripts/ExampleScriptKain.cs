5705b55887915fd8bca4940bbcd3f824814625fb
Kainkun <kainkun1000@gmail.com>
2019-04-05 17:46:53 -0700

    better example

--- UnityLongTermGameJam1/Assets/Scripts/ExampleScriptKain.cs
+++ UnityLongTermGameJam1/Assets/Scripts/ExampleScriptKain.cs
@@ -7,6 +7,9 @@ public class ExampleScriptKain : MonoBehaviour
     public float MoveSpeed;
     public float SinSpeed;
     public float SinWidth;
+
+    public Vector3 goToPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,22 @@ public class ExampleScriptKain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(MoveSpeed / 100, Mathf.Sin(Time.time * SinSpeed) * SinWidth / 100, 0);
+
+        goToPosition.x += MoveSpeed * Time.deltaTime;
+        goToPosition.y = Mathf.Sin(Time.time * SinSpeed) * SinWidth;
+
+        transform.position = goToPosition;
+
+
+
+
+
+
+        //transform.position += new Vector3(MoveSpeed * Time.deltaTime, Mathf.Sin(Time.time * SinSpeed) * SinWidth * Time.deltaTime, 0);
+        
+        
+        
+        //transform.position += new Vector3(MoveSpeed / 100, Mathf.Sin(Time.time * SinSpeed) * SinWidth / 100, 0);
     }
 
 }
