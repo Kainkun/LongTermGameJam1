576b0479451a464984714a4bdca1505845d5bc02
rho-selynn <conrady.roselynn@gmail.com>
2019-04-05 19:33:17 -0700

    w/ Kain's help, got Box Enemy to oscillate up and down while moving right to left

--- UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
+++ UnityLongTermGameJam1/Assets/Scripts/BoxEnemyIntenseEyebrows.cs
@@ -4,32 +4,28 @@ using UnityEngine;
 
 public class BoxEnemyIntenseEyebrows : MonoBehaviour
 {
-    public Vector3 targetPosition; // declaring the Vector3 variable
-   
-    public float speed = 10; // declaring speed (fps) 
+    public float MoveSpeed; // declaring variables
+    public float SinSpeed;
+    public float SinWidth;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-        Debug.Log("My goal is to get the sprite to move in the -x direction!");
+    Vector3 startPosition;
+    Vector3 goToPosition;
 
-        //targetPosition = transform.position; // This didn't work
-        transform.position = new Vector3(10, 0); // putting the sprite at (10,0)
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        goToPosition = transform.position;
     }
 
-	// Update is called once per frame
-	void Update()
-	{
-        if (transform.position.x > 0) // if sprite position is greater than 0 in x-direction
-        {
-            targetPosition.x = -10; // target position will be -10 in x-direction
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        goToPosition.x -= MoveSpeed * Time.deltaTime;
+        goToPosition.y = startPosition.y + Mathf.Sin(Time.time * SinSpeed) * SinWidth;
 
-         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime); // Vector3 will move to target position
-                                                                                                               // from orginal position based on change in time
-                                                                                                               // between the current and last frame
+        transform.position = goToPosition;
+        
 
-        // transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed); // Without Time.deltaTime, the sprite moves
-                                                                                                //in negative x-direction based on the declared "speed"
     }
 }
