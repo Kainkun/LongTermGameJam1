9052647ecbdf5b5be04e39a816b4b043296c8827
B-Nicole <hughesb791@gmail.com>
2019-04-15 10:24:12 -0700

    Movement and Dash setup

---
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyDash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrittanyDash : MonoBehaviour
+{
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector3 vel = rb.velocity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
