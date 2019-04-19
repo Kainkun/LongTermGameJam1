436ebc67c04f046c6669344e6f24af6b0b5b8c43
B-Nicole <hughesb791@gmail.com>
2019-04-19 14:30:56 -0700

    Dash

--- UnityLongTermGameJam1/Assets/Scripts/BrittanyDash.cs
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyDash.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BrittanyDash : MonoBehaviour
 {
+    public Vector3 velocity;
+    public Vector3 previous; //Vector3 is X Y Z position 
+    public float distance; 
+                            
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +17,16 @@ public class BrittanyDash : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocity = (transform.position - previous) / Time.deltaTime; //get diffrence of current player position and previous position(last frame), which gives a velocity
+        velocity.Normalize(); // Normalize the players velocity to get a direction without magnitude
+        previous = transform.position; //get the current position which will be used for the next frames previous position
+
+        velocity *= distance; // We are multiplying the normalized circle with distance to determine how far the object teleports and in what direction
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // print("Pressing key");
-            transform.position += new Vector3(-5, 0, 0); //X , Y , Z
+            transform.position += velocity; //X , Y , Z
         }
         
     }
