09b8319468d02e7aa5cf79d21b33ce59a28ae78e
rho-selynn <conrady.roselynn@gmail.com>
2019-04-18 22:24:51 -0700

    Fixed compiler errors within Enemy Health script

--- UnityLongTermGameJam1/Assets/Scripts/EnemyHealth.cs
+++ UnityLongTermGameJam1/Assets/Scripts/EnemyHealth.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
-{   int health = 10
+{
+    int health = 10;
     void speak()
 
     {
         Debug.Log("In Bulgaria, nodding means 'no' and a head shake means 'yes'");
-        health = health - 1;
-        return health;
+        //health = health - 1;
+        //return health;
     }
     
     // Start is called before the first frame update
