97a15bd4485e1bd3d3cbba4dd3b0e28ddc6bccc5
B-Nicole <hughesb791@gmail.com>
2019-04-30 20:57:25 -0700

    Parallax Background
    Attempting the parallax background

---
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyParallaxBackground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrittanyParallaxBackground : MonoBehaviour
+{
+    /*
+     * @author Brittany Hughes
+     * @Date April 26, 2019
+     * Parralax background scrolling
+     * */
+    public float imageWidth;
+    public float speed;
+
+    GameObject backgroundOne;
+    GameObject backgroundTwo;
+
+    private void Start()
+    {
+        
+    }
+     void FixedUpdate()
+    {
+         transform.position += new Vector3(-speed * Time.deltaTime, 0);
+        if(transform.position.x < 10)
+        {
+            transform.position = new Vector3(10, 0, 0);
+        }
+    }
+     
+
+}
