157c5c0877d5ee074e6ae45d175ef4539f9e007e
Carter <andrewscart@gmail.com>
2019-05-02 16:30:19 -0700

    framework for the end of the game
    ending scene, credits, boss scene, boss enemy added

---
+++ UnityLongTermGameJam1/Assets/EndingManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndingManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void credits()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
