3be381cca58a564b67086302e0efe9e4c3722246
B-Nicole <hughesb791@gmail.com>
2019-05-22 16:07:32 -0700

    Fade
    Fading between levels

---
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyLevelChanger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BrittanyLevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // print("space key was pressed");
+            FadeToNextLevel();
+
+        }
+    }
+    public void FadeToNextLevel()
+    {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
