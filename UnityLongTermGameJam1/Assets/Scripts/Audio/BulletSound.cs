31835d3a1c6f3f0d57d93a9646595ad278a947d3
MSI\cyber <darkforcesii@gmail.com>
2019-06-04 19:34:58 -0700

    Added Audio
    Added script to play bullet sound after hitting space. Also set up some logic in Unity to balance the audio while the game is played.

---
+++ UnityLongTermGameJam1/Assets/Scripts/Audio/BulletSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
+public class BulletSound : MonoBehaviour
+{
+    AudioSource cybepunkBullets;
+
+    public AudioClip CyberpunkGunshot;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.PlayOneShot(CyberpunkGunshot, 0.5F);
+        }
+        
+    }
+}
---
+++ UnityLongTermGameJam1/Assets/Scripts/Audio/CyberpunkLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CyberpunkLoop : MonoBehaviour
+{
+    AudioSource cyberpunkMainTheme;
+    public AudioClip CyberPunkMainTheme;
+
+    void Start()
+    {
+        cyberpunkMainTheme = GetComponent<AudioSource>();
+        cyberpunkMainTheme.PlayDelayed(6.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
