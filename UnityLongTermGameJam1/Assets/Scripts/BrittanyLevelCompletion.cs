77d92839a543b4f9b6786171fd7366dd0441afa4
B-Nicole <hughesb791@gmail.com>
2019-06-08 14:47:04 -0700

    UI Level Completion
    Created the beginning of a script that calculates the completion of a level

--- UnityLongTermGameJam1/Assets/Scripts/BrittanyLevelCompletion.cs
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyLevelCompletion.cs
@@ -8,10 +8,10 @@ public class BrittanyLevelCompletion : MonoBehaviour
     public float time;
     public float LevelLength;
     private float percent;
-   
+
     void Start()
     {
-        
+        print("Starting");
     }
 
     // Update is called once per frame
@@ -21,10 +21,11 @@ public class BrittanyLevelCompletion : MonoBehaviour
         //if time is greater than levelLength the level should be over
         if (time > LevelLength)
         {
+            print("The level is over");
             // the level is over
         }
-        percent = time / LevelLength;
-        GetComponent<Text>().text = percent.ToString();
+       percent = time / LevelLength;
+       GetComponent<Text>().text = percent.ToString();
 
     }
 }
