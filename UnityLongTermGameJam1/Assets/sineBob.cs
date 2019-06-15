9e4ba8e4030710e858b9bcb933680ed3918e3331
Kainkun <kainkun1000@gmail.com>
2019-06-14 18:36:41 -0700

    implemented nassems bg and added bobing

---
+++ UnityLongTermGameJam1/Assets/sineBob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sineBob : MonoBehaviour
+{
+    float startY;
+    public float height;
+    public float speed;
+    public float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y = startY + Mathf.Sin(Time.time * speed + offset) * height;
+        transform.position = pos;
+    }
+}
