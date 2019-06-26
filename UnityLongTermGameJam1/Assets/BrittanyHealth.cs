38e0cd2a38f9acdd1da93c3f29cc636dba2f43b2
B-Nicole <hughesb791@gmail.com>
2019-06-26 15:55:16 -0700

    Hearts
    Rough draft of the heart health system

---
+++ UnityLongTermGameJam1/Assets/BrittanyHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BrittanyHealth : MonoBehaviour
+{
+    
+    public int health;
+    public int numOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+ 
+}
---
+++ UnityLongTermGameJam1/Assets/Scripts/BrittanyHealthHearts.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrittanyHealthHearts : MonoBehaviour
+{
+    private int maxHeartAmount = 3;
+    public int startHearts = 3;
+    public int currentHealth;
+    private int maxHealth;
+    private int healthPerHeart = 3;
+
+    public Image[] healthImages;
+    public Sprite[] healthSprites;
+
+    void Start()
+    {
+        currentHealth = startHearts * healthPerHeart;
+        maxHealth = maxHeartAmount * healthPerHeart;
+        checkHealthAmount();
+    }
+
+   void checkHealthAmount()
+    {
+        for(int i = 0; i < maxHeartAmount; i++)
+        {
+            if(startHearts <= i)
+            {
+                healthImages[i].enabled = false;
+            }
+            else
+            {
+                healthImages[i].enabled = true;
+            }
+        }
+        UpdateHearts();
+    }
+    void UpdateHearts()
+    {
+        bool empty = false;
+        int i = 0;
+        foreach(Image image in healthImages)
+        {
+            if (empty)
+            {
+                image.sprite = healthSprites[0];
+            }
+            else
+            {
+                i++;
+                if(currentHealth >= i * healthPerHeart)
+                {
+                    image.sprite = healthSprites.[healthSprites.Length - 1];
+                }
+                else
+                {
+                    int currentHeartHealth = (int)(healthPerHeart - (healthPerHeart * i - currentHealth));
+                    int healthPerImage = healthPerHeart / (healthSprites.Length - 1);
+                    int imageIndex = currentHeartHealth / healthPerImage;
+                    image.sprite = healthSprites[imageIndex];
+                    empty = true; 
+                }
+            }
+        }
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, startHearts * healthPerHeart);
+        UpdateHearts();
+    }
+    public void AddHeartContainer()
+    {
+        startHearts++;
+        startHearts = Mathf.Clamp(startHearts, 0, maxHeartAmount);
+        //currentHealth = startHearts * healthPerHeart;
+        //maxHealth = maxHeartAmount * healthPerHeart;
+        checkHealthAmount();
+    }
+}
