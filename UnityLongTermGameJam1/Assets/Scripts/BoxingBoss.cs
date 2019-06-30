298828be9a2cc9984d843d8b35dcbc69e9273ade
Jacob Golden-Needham <jacob@goldenneedham.com>
2019-06-29 18:03:11 -0700

    Worked on boss a bit

---
+++ UnityLongTermGameJam1/Assets/Scripts/BoxingBoss.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingBoss : MonoBehaviour
+{
+
+    public SpriteRenderer[] fists;
+    public SpriteRenderer head;
+    public SpriteRenderer body;
+    public SpriteRenderer armor;
+
+    public float timeBetweenPunches = 1;
+    public float fistTrackTime = 1;
+    public float fistSpeed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Stage1(){
+
+
+    }
+
+    IEnumerator punch(){
+        yield return null;
+    }
+}
