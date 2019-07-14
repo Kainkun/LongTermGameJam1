8053fe1e1fe2b5fb26c74745c2836a92364376ba
MSI\cyber <darkforcesii@gmail.com>
2019-07-13 22:24:32 -0700

    Added Sounds
    Added EnemyShooting and EnemyDamage Sounds

---
+++ UnityLongTermGameJam1/Assets/Scripts/Audio/EnemyDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    public AudioClip enemyDamage;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "PlayerBullet")
+        {
+            GetComponent<AudioSource>().PlayOneShot(enemyDamage);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
