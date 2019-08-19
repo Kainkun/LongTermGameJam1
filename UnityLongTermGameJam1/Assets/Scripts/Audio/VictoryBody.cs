9ba01559632bcaff593c475c6e5db21e930ead1d
MSI\cyber <darkforcesii@gmail.com>
2019-08-18 20:13:23 -0700

    Added Victory Theme

---
+++ UnityLongTermGameJam1/Assets/Scripts/Audio/VictoryBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryBody : MonoBehaviour
+{
+    AudioSource VictorySource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().PlayDelayed(17.455f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
